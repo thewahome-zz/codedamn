874835367cf17ad0a74ae390e476a2ba72e50c97
Charles Wahome <cwahome@virtualcity.co.ke>
2016-10-26 16:38:04 +0300

    Chapter 12: Introduction to routing

---
+++ js/controller.js
@@ -0,0 +1,13 @@
+var app = angular.module('mainApp',['ngRoute']);
+app.config(function($routeProvider){
+	$routeProvider
+	.when('/',{
+		template: 'Welcome user!'
+	})
+	.when('/anotherPage',{
+		template: 'Welcome user! Again'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+});
