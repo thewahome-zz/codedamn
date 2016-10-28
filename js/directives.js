7cac231126e0604e892d4ff048e68a43c50cd035
Charles Wahome <cwahome@virtualcity.co.ke>
2016-10-28 09:16:28 +0300

    Chapter 38: Custom Directives - Introduction

---
+++ js/directives.js
@@ -0,0 +1,12 @@
+/**
+* mainApp Module
+*
+* Description: Directives
+*/
+var app = angular.module('mainApp', []);
+
+app.directive('myFirstDirective',function(){
+	return {
+		template:'HW Hellow World!'
+	};
+});
