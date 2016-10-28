1ced7c104d2650f1bddd1faa0d6afaf801a7ff44
Charles Wahome <cwahome@virtualcity.co.ke>
2016-10-28 09:26:01 +0300

    Chapter 39: Directive Definition Object Properties

--- js/directives.js
+++ js/directives.js
@@ -7,6 +7,7 @@ var app = angular.module('mainApp', []);
 
 app.directive('myFirstDirective',function(){
 	return {
-		template:'HW Hellow World!'
+		template:'HW Hellow World!',
+		restrict:'A'
 	};
 });
