59eb1adae9751aa0f693a92a6789072279015eec
Charles Wahome <cwahome@virtualcity.co.ke>
2016-10-28 06:36:31 +0300

    Chapters 29 - 35: Creating a to do app

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ app.provider('fromProvider',function(){
 });
 
 app.controller('fromSFPCtrl',function($scope,fromService,fromFactory,fromProvider){
-  $scope.greetingsMessage=[fromService.message,fromFactory.message,fromProvider.message];
+	$scope.greetingsMessage=[fromService.message,fromFactory.message,fromProvider.message];
 });
 
 app.controller('ngShowDirectiveCtrl',function($scope){
@@ -35,5 +35,43 @@ app.controller('ngShowDirectiveCtrl',function($scope){
 	$scope.toggle=function(){
 		$scope.show = $scope.show == 'firstMessage' ? 'secondMessage' : 'firstMessage';
 	};
+});
 
+app.controller('todoAppCtrl',function($scope){
+	$scope.tasks = [];
+	var taskData = localStorage['tasksList'];
+
+	if (taskData!==undefined) {
+		$scope.tasks=JSON.parse(taskData);
+	}
+	$scope.searchEnter=function() {
+		if(event.which==13 && $scope.task != ''){
+			$scope.addTask();
+		}
+	};
+	
+	$scope.addTask=function(){
+		$scope.tasks.push({'taskMessage':$scope.task,'status':false});
+		$scope.task='';
+		localStorage['tasksList'] = JSON.stringify($scope.tasks);
+		console.log(localStorage);
+	};
+
+	$scope.contentEdit = function(message){
+		for (i = 0; i < $scope.tasks.length; i++) {
+			if ($scope.tasks.taskMessage==message) {
+				$scope.tasks[i].taskMessage = event.target.innerText;
+			}
+		}
+
+		localStorage['tasksList'] = JSON.stringify($scope.tasks);
+
+		event.target.contentEditable = event.target.contentEditable == "false" ? "true" : "false";
+	};
+
+	$scope.enterAgain=function(message){
+		if(event.which==13 && message != ''){
+			$scope.contentEdit(message);
+		}
+	};
 });
