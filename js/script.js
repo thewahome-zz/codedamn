f18d9dde7cbbc61035871b0d71b222c2e13c7243
Charles Wahome <cwahome@virtualcity.co.ke>
2019-07-06 00:36:07 +0300

    Chapter 27: Services V/s Factories V/s Providers

---
+++ js/script.js
@@ -0,0 +1,31 @@
+/**
+*  Module
+*
+* Description
+*/
+var app= angular.module('mainApp', []);
+
+app.service('fromService',function(){
+	this.message="This is from the service";
+});
+
+app.factory('fromFactory', function(){
+	var factory={};
+	factory.message="This is from the factory";
+	return factory;
+});
+
+app.provider('fromProvider',function(){
+	var providerMessage="This is from the provider";
+	return{
+		$get:function(){
+			return{
+				message:providerMessage
+			}
+		}
+	}
+});
+
+app.controller('fromSFPCtrl',function($scope,fromService,fromFactory,fromProvider){
+  $scope.greetingsMessage=[fromService.message,fromFactory.message,fromProvider.message];
+});
